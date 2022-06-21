091fb1e80eb041da7cb0b34c426facef6aaac7fd
Sajaldeb25 <sajaldebnath02@gmail.com>
2022-06-21 16:29:07 +0600

    frontend change to normal hellow vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,25 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <!-- <div id="app"> -->
+
+    {{msg}}
+  <!-- </div> -->
+  <!-- <img alt="Vue logo" src="./assets/logo.png">
+  <HelloWorld msg="Welcome to Your Vue.js App"/> -->
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  // components: {
+  //   HelloWorld
+  // }
+  data(){
+    return{
+      msg:'Hello Vue js'
+    }
+
   }
 }
 </script>
