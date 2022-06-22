1542a3d90f6388ca7a36caebb77872fd9e91dbab
Sajaldeb25 <sajaldebnath02@gmail.com>
2022-06-22 13:51:30 +0600

    Post method created in view page for creating new students info

--- backend/students/views.py
+++ backend/students/views.py
@@ -14,3 +14,10 @@ class StudentsView(APIView):
         student = Students.objects.all()
         serializer = StudentSerializer(student, many=True)
         return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def post(self, request, format=None):  # to create student info
+        serializer = StudentSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
